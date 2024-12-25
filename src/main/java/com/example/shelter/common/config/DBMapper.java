bbd4a23dc87c2d33744606eddf6996a048ca2e8d
ptg01 <ptg0131@gmail.com>
2024-12-25 15:45:01 +0900

    초기세팅

---
+++ src/main/java/com/example/shelter/common/config/DBMapper.java
@@ -0,0 +1,10 @@
+package com.example.shelter.common.config;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface DBMapper {
+
+}
